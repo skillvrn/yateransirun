2b5335a7dc883cf0880d0ee5f449abe25a51effc
Danil Kuznetsov <skillvrn@gmail.com>
2023-04-25 16:07:45 +0300

    Try test 02

--- terraform/modules/tf-yc-instance/main.tf
+++ terraform/modules/tf-yc-instance/main.tf
@@ -1,7 +1,3 @@
-data "yandex_compute_image" "my_image" {
-  family = "ubuntu-2004-lts"
-}
-
 resource "yandex_compute_instance" "vm" {
   count         = length(var.vm_suffixes)
   name          = "${var.instance_name}-${element(var.vm_suffixes, count.index)}"
